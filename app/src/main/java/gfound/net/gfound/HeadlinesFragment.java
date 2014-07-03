8e4a7550b95dd67a82bae299d2549b50ef646bc6
吴海军 <whjmvp7237@163.com>
2014-07-03 16:58:50 +0800

    添加Fragment1个

--- app/src/main/java/gfound/net/gfound/HeadlinesFragment.java
+++ app/src/main/java/gfound/net/gfound/HeadlinesFragment.java
@@ -11,7 +11,6 @@ public class HeadlinesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_main, container, false);
     }
 }
